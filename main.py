12acf75566498e8abdc86dd958b73ba1e0861d9e
Adek06 <adek06@163.com>
2017-08-30 16:20:53 +0800

    完善了点小细节。 选择非数字时的循环和退出提示 单独分离了下载和创建文件夹部分

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import Search_eps, Ep_infos, Downloads, Creat_folders
 
+
+def creat_folder(animeName):
+    creatEpFolder = Creat_folders.creat_folder(animeName)
+    creatEpFolder.creat_ep_folder()
+
+def download_ep(enInfo,animeName):
+    downLoad = Downloads.Download(epInfo,animeName)
+    downLoad.down_load()
+
 def main():
 
     print("""
@@ -7,46 +16,52 @@ def main():
     ####                                                          ####
     ####                                                          ####
     ####                                                          ####
-    ####                                                          ####  
-    ####                    视频下载器                              ####
-    ####                                                          ####          
+    ####                                                          ####
+    ####                    视频下载器                            ####
+    ####                                                          ####
     ####                                      v:1.0               ####
     ####                                                          ####
-    ####                                     by：不愿透露姓名的游先生 ####
+    ####                                 by：不愿透露姓名的游先生 ####
     ##################################################################
     """)
 
-    print('请问需要在哪里下载视频？')
-    
+    print("""
+    在开始下载前，请确保已经下载并安装了Chrome浏览器或者Firefox浏览器，
+    并在指定的位置放置好对应浏览器的driver。
+    """)
+
+    print("""
+    请问需要在哪里下载视频？
+    """)
+
     print("""
     1.bilibili
     """)
 
-    select = input(">>> ")
     try:
-        if select == '1':
-            
-            search = Search_eps.search_ep()
-            try:
-                animeDns,animeName = search.bilibili_anime_search()
-            except:
-                print("在搜索出了问题,请尝试查询该动漫是否属于b站番组计划")
-                exit()
-                
-            getEpInfo = Ep_infos.Ep_info(animeDns)
-            epInfo = getEpInfo.bilibili_Ep_info()
+        notNum = True
+        while(notNum):
+            select = input(">>> ")
+            if select == '1':
+                notNum = False
+                try:
+                    animeDns,animeName = Search_eps.search_ep().bilibili_anime_search()
+                except:
+                    print("在搜索出了问题,请尝试查询该动漫是否属于b站番组计划")
+                    exit()
 
-            try:
-                creatEpFolder = Creat_folders.creat_folder(animeName)
-                creatEpFolder.creat_ep_folder()
-            except:
-                print('在创建文件夹时出了点问题')
-                exit()
+                getEpInfo = Ep_infos.Ep_info(animeDns)
+                epInfo = getEpInfo.bilibili_Ep_info()
 
-            downLoad = Downloads.Download(epInfo,animeName)
-            downLoad.down_load()
-    except:
-        pass
+                creat_folder(animeName)
+
+                downLoad_ep(enInfo,animeName)
+            else :
+                print("请输入数字，否则按\"Ctrl+C\"退出")
+
+    except KeyboardInterrupt:
+        print("\n")
+        print("成功退出程序")
 
 if __name__ == "__main__":
     main()
