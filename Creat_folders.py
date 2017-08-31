c0c5d952bd8a930b62cffa8b1aac3cbfa1368982
Adek06 <adek06@163.com>
2017-08-31 17:33:46 +0800

    增加了错误提示

--- Creat_folders.py
+++ Creat_folders.py
@@ -4,16 +4,17 @@ class creat_folder(object):
 
 	def __init__(self, Ep_name):
 		self.Ep_name = Ep_name
-	
-	def creat_ep_folder(self):
-		
-		if 'EP' in (listdir(".")):
-			pass
-		else:
-			mkdir('./EP/')
 
-		if self.Ep_name in (listdir('./EP/')):
-			pass
-		else:
-			mkdir('./EP/'+str(self.Ep_name))
-		
+	def creat_ep_folder(self):
+		try:
+			if 'EP' in (listdir(".")):
+				pass
+			else:
+				mkdir('./EP/')
+
+				if self.Ep_name in (listdir('./EP/')):
+					pass
+				else:
+					mkdir('./EP/'+str(self.Ep_name))
+		except:
+			print("在创建文件夹时出了问题")
