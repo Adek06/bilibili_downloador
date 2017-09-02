ed1ceeb4025f436443e44da893a76e72548d5a5e
Adek06 <adek06@163.com>
2017-09-02 11:54:45 +0800

    更改浏览器退出的时机

--- Ep_infos.py
+++ Ep_infos.py
@@ -8,16 +8,18 @@ class Ep_info(object):
         self.html = html
 
     def bilibili_Ep_info(self):
+        print('正在打开浏览器，请稍等...')
         driver = webdriver.Firefox()
         try:
             driver.get(self.html)
         except:
             print("打开浏览器这里出了问题")
-        print('在全力打开中，请稍等...')
+            exit()
         sleep(5)
         Ep_info_rep = findall(r"bangumi.bilibili.com/anime/\d{2,}/play#\d{2,}",driver.page_source)
         Ep_info_list = []
         driver.close()
+        print("浏览器正常关闭...")
         for i in Ep_info_rep:
             if i not in Ep_info_list:
                 Ep_info_list.append(i)
