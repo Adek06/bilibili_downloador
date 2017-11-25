d9f9dee736bb120feaeb0278199f2af773d5d4ad
Adek06 <adek06@163.com>
2017-11-25 17:37:34 +0800

    修复了搜索动漫时无法显示全部名称的问题

--- Search_eps.py
+++ Search_eps.py
@@ -17,14 +17,14 @@ class search_ep(object):
         name = input('>>> ')
         anime_url = 'https://search.bilibili.com/all?keyword='+name
         anlh = get(anime_url)
-        anlhm = findall(r"class=\"title\" href.{2,}title=\".{2,20}\"",anlh.text)
+        anlhm = findall(r"class=\"title\" href.{2,}title=\".{2,40}\" lnk",anlh.text)
         anl = []
         for i in range(len(anlhm)):
-            anl.append(search(r'title=\".{2,15}\" lnk',anlhm[i]).group(0)[7:-5])
+            anl.append(search(r'title=\".{2,40}\" lnk',anlhm[i]).group()[7:-5])
 
         print("你要找的是下面哪一个动漫呢？(输入名字前的序号选择)")
         for i in range(len(anl)):
-            print(str(i)+"、"+anl[i])
+            print(str(i)+"、"+str(anl[i]))
         asn = input(">>> ")
         anime_address_match = search(r"//.{1,}\?",anlhm[int(asn)]) 
         adns = "https:"+anime_address_match.group(0)[:-1]
