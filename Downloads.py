5bb7061a11a0ded03fa282b567f219bff5a958a7
Adek06 <adek06@163.com>
2017-12-30 14:31:03 +0800

    最后显示下载失败的集数和地址

--- Downloads.py
+++ Downloads.py
@@ -1,4 +1,5 @@
 from os import system, chdir, remove, getcwd
+from os import name as systemName
 import sys
 from you_get import common as you_get
 import multiprocessing as mp
@@ -8,69 +9,64 @@ class Download(object):
 
     def __init__(self,Ep_info_list):
         self.Ep_info_list = Ep_info_list
-        #self.Ep_name = Ep_name
 
-    def use_youget(self,down_url_list):
-        redown=[]
-        for i in range(len(down_url_list)):
+    def clearTerminal(self):
+        if systemName =='nt':
+            system('cls')
+        elif systemName == 'posix':
+            system('clear')
+
+    def creatDict(self,listT,st,end):
+        dictT = {}
+        for i,j in zip(range(st,end+1),listT):
+            dictT[i] = j
+
+        return dictT
+
+    def use_youget(self,down_url_dict):
+        redownDict={}
+        for i in list(down_url_dict.keys()):
             try:
-                sys.argv=['you-get',down_url_list[i]]
+                sys.argv=['you-get',down_url_dict[i]]
                 you_get.main()
-                system('cls')
+                self.clearTerminal()
             except:
                 print("""
 
 
                 """)
-                print("下载{}的过程出了点问题，稍后会重新下载".format(down_url_list[i]))
+                print("下载{}的过程出了点问题，稍后会重新下载".format(down_url_dict[i]))
                 print("""
 
 
                 """)
-                redown.append(down_url_list[i])
-        return redown
+                redownDict[i] = down_url_dict[i]
+        return redownDict
+
+
+
 
 
     def down_load(self):
         print("请问要从第几集开始下载？")
-        startEp_id = input(">>> ")
+        startEp_id = int(input(">>> "))
         print("要下载到第几集？")
-        endEp_id = input(">>> ")
-        down_list = self.Ep_info_list[int(startEp_id)-1:int(endEp_id)]
+        endEp_id = int(input(">>> "))
+        down_list = self.Ep_info_list[startEp_id-1:endEp_id]
+        down_dict = self.creatDict(down_list,startEp_id,endEp_id)
         print("马上开始下载，请稍后...")
-        #chdir('.\EP')
-        #chdir(str(self.Ep_name))
 
-        redown = self.use_youget(down_list)
+        redown = self.use_youget(down_dict)
         tryNum = 0
         while len(redown) != 0 and tryNum < 5:
             redown = self.use_youget(redown)
             tryNum += 1
         
-        
-        '''
-        down_list1=[]
-        down_list2=[]
-
-        for i in range(len(down_list)):
-            if i%2==0:
-                down_list1.append(down_list[i])
-            else:
-                down_list2.append(down_list[i])
-
-        pr1 = mp.Process(target=self.use_youget,args=(down_list1,))
-        pr1.start()
-        if (len(down_list2) !=0):
-            pr2 = mp.Process(target=self.use_youget,args=(down_list2,))
-            pr2.start()
-            pr2.join()
-        pr1.join()
-        '''
-        system('cls')
+        self.clearTerminal()
         if len(redown)>1:
             print("对不起，下载无能为力，请等以后的版本")
             print("没能下载的有一下几集：")
-            print(redown)
+            print(str(redown))
         removeList = glob(r"./*.xml")
         for x in removeList:
             remove(x)
