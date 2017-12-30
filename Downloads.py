1c6152a9a94516f6054dbe8fd0db8460fdf8612a
Adek06 <adek06@163.com>
2017-12-30 15:58:15 +0800

    修改部分函数的名字

--- Downloads.py
+++ Downloads.py
@@ -4,32 +4,33 @@ import sys
 from you_get import common as you_get
 import multiprocessing as mp
 from glob import glob
+import pickle
 
 class Download(object):
 
     def __init__(self,Ep_info_list):
         self.Ep_info_list = Ep_info_list
-
-    def clearTerminal(self):
+    def __clearTerminal(self):
         if systemName =='nt':
             system('cls')
         elif systemName == 'posix':
             system('clear')
 
-    def creatDict(self,listT,st,end):
+    def __creatDict(self,listT,st,end):
         dictT = {}
         for i,j in zip(range(st,end+1),listT):
             dictT[i] = j
 
         return dictT
 
-    def use_youget(self,down_url_dict):
+    def __use_youget(self,down_url_dict):
         redownDict={}
         for i in list(down_url_dict.keys()):
             try:
                 sys.argv=['you-get',down_url_dict[i]]
                 you_get.main()
-                self.clearTerminal()
+                self.__clearTerminal()
+            
             except:
                 print("""
 
@@ -41,11 +42,8 @@ class Download(object):
 
                 """)
                 redownDict[i] = down_url_dict[i]
+            
         return redownDict
-
-
-
-
 
     def down_load(self):
         print("请问要从第几集开始下载？")
@@ -53,16 +51,16 @@ class Download(object):
         print("要下载到第几集？")
         endEp_id = int(input(">>> "))
         down_list = self.Ep_info_list[startEp_id-1:endEp_id]
-        down_dict = self.creatDict(down_list,startEp_id,endEp_id)
+        down_dict = self.__creatDict(down_list,startEp_id,endEp_id)
         print("马上开始下载，请稍后...")
 
-        redown = self.use_youget(down_dict)
+        redown = self.__use_youget(down_dict)
         tryNum = 0
         while len(redown) != 0 and tryNum < 5:
-            redown = self.use_youget(redown)
+            redown = self.__use_youget(redown)
             tryNum += 1
         
-        self.clearTerminal()
+        self.__clearTerminal()
         if len(redown)>1:
             print("对不起，下载无能为力，请等以后的版本")
             print("没能下载的有一下几集：")
